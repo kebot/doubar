160a45c0b1de5e2d3a9b269845085aa1c86d2e80
Feng Yao <feng.yao@deepl.com>
2025-10-10 16:39:23 +0200

    init tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/core";
-
 import "./App.css";
 
 function App() {
-  // const [greetMsg, setGreetMsg] = useState("");
-  // const [name, setName] = useState("");
-
-  // async function greet() {
-  //   // Calling rust function from the frontend
-  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
-
   return (
-    <main className="container">
+    <main className="container bg-transparent">
       &nbsp; Welcome to TauriBar
     </main>
   );
